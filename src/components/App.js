16f7c43b84ee412f7711baccbaa78d14278cba62
Kazuki Takane <highjumptakane@gmail.com>
2023-05-28 17:09:27 +0900

    add todolist

--- src/components/App.js
+++ src/components/App.js
@@ -36,10 +36,28 @@ function App() {
       <textarea />
 
       <button>+ TODOを追加</button>
-      <h2>TODOリスト</h2>
+      <h2>未完了TODOリスト</h2>
       <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.content}({todo.done ? "完了" : "未完了"})</li>
+        {inCompletedList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}
+            <button>
+              {todo.done ? "未完了リストへ" : "完了リストへ"}
+            </button>
+            <button>削除</button>
+          </li>
+        ))}
+      </ul>
+      <h2>完了TODOリスト</h2>
+      <ul>
+        {CompletedList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}
+            <button>
+              {todo.done ? "未完了リストへ" : "完了リストへ"}
+            </button>
+            <button>削除</button>
+          </li>
         ))}
       </ul>
     </>
